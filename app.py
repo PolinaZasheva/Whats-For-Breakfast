705d2011e645dba1813f8745e3b1eb55ed71ce33
David <42821567+DavidJeffPhillips@users.noreply.github.com>
2019-07-20 16:09:19 -0500

    app.py update, indexhtml and result html

---
+++ app.py
@@ -0,0 +1,28 @@
+
+from food_predict import predict_class
+from food_predict import model_best
+from ImageResize import resizer
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    if request.method == 'GET':
+        return render_template('index.html', value='hi')
+    if request.method == 'POST':
+        print(request.files)
+        if 'file' not in request.files:
+            print('file not uploaded')
+            return
+        file = request.files['file']
+        image = file.read()
+        breakfast_name = predict_class(model_best, images=image)
+        # image = resizer(images=image)
+        predict_class(model_best, images=image)
+        # print(resizer(images=image))
+        return render_template('result.html', breakfast=breakfast_name)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
